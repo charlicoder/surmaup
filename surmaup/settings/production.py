8d4334851a0e9b3f5004fb198686a8c4e1429fe3
Mamun Khondoker <mamun1980@gmail.com>
2020-05-18 05:07:30 +0600

    updated settings file for prod server

--- surmaup/settings/production.py
+++ surmaup/settings/production.py
@@ -1,6 +1,11 @@
 from .base import *
 
-
+import os
+from django.utils.translation import ugettext_lazy as _
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
+    
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
