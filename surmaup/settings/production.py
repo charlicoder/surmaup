a899b3a446cc39125001beaab8e6c30b8dc07d5f
Mamun Khondoker <mamun1980@gmail.com>
2020-05-18 05:04:14 +0600

    updated settings file for prod server

--- surmaup/settings/production.py
+++ surmaup/settings/production.py
@@ -3,6 +3,8 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['*']
+
 STATIC_ROOT = (
     os.path.join(BASE_DIR, "statics"),
 )
@@ -17,5 +19,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
 
 print('Loaded production environment.....!')
