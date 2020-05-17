951206052852343f268c4a618510a9a21bbc0b13
Mamun Khondoker <mamun1980@gmail.com>
2020-05-18 05:45:51 +0600

    added staticfiles directory

--- surmaup/urls.py
+++ surmaup/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="base.html"), name="home"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # urlpatterns += i18n_patterns(
 #     url(r’^{}/’.format(settings.DJANGO_ADMIN_URL), admin.site.urls),
