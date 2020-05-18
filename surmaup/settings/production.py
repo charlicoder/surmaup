11be0543e483b2c12c1686ac5c4d1c67c109d07d
charlicoder <charlicoder@gmail.com>
2020-05-18 07:12:16 +0600

    changed settings

--- surmaup/settings/production.py
+++ surmaup/settings/production.py
@@ -7,6 +7,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'surmaup_static')
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
